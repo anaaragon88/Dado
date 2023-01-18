65bb0a26ab00c661215188b1b4aa3195b848c0c7
Ana Aragon <ana.aragon88@gmail.com>
2023-01-18 13:10:27 +0100

    subir archivos

---
+++ script.js
@@ -0,0 +1,7 @@
+let numero_aleatorio = Math.floor(Math.random() * 6) + 1;
+
+alert(numero_aleatorio);
+
+let img_dado = "img/" + numero_aleatorio + ".png";
+
+document.querySelector('.dado_img').setAttribute('src', img_dado);
