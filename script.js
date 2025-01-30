6decc823d511da9e6ce5fd572e415445846662d0
Ana Aragon <106325314+anaaragon88@users.noreply.github.com>
2025-01-31 00:47:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let numero_aleatorio = Math.floor(Math.random() * 6) + 1;
 
 alert(numero_aleatorio);
 
-let img_dado = "img/" + numero_aleatorio + ".png";
+//let img_dado = "img/" + numero_aleatorio + ".png";
+let img_dado = `img/${numero_aleatorio}.png`;
+
 
 document.querySelector('.dado_img').setAttribute('src', img_dado);
